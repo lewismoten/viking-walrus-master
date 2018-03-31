f8c30b534cba2515abbab4b4e45ec179707d02ef
Lewis Moten <lewismoten@gmail.com>
2018-03-30 21:30:50 -0500

    add emoji icons to static folder listings

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ function requestCallback(request, response) {
       if (error.code === 'EISDIR') {
         showFolderContents(response, localPath);
       } else {
-        response.writeHead(HTTP_STATUS_INTERNAL_SERVER_ERROR, getHeaders());
-        response.end(`ERROR: ${error.message}`, ENCODING);
+        response.writeHead(HTTP_STATUS_INTERNAL_SERVER_ERROR, getHeaders('?.html'));
+        response.end(`<h1>\u{1F6D1}ERROR</h1><p>${error.message}</p>`, ENCODING);
         console.error(error);
       }
     } else {
@@ -35,13 +35,15 @@ function requestCallback(request, response) {
 function showFolderContents(response, folderPath) {
   fs.readdir(folderPath, (err, files) => {
     if (err) {
-      response.writeHead(HTTP_STATUS_INTERNAL_SERVER_ERROR, getHeaders());
+      response.writeHead(HTTP_STATUS_INTERNAL_SERVER_ERROR, getHeaders('?.html'));
       response.end(`Can not read folder: ${folderPath}\r\n${err}`, ENCODING);
     } else {
       response.writeHead(HTTP_STATUS_OK, getHeaders('?.html'));
+      response.write('<h1>Folder Contents</h1>', ENCODING);
+      response.write(`<h2>\u{1F4C2}${folderPath}</h2>`, ENCODING);
       response.write('<ol>', ENCODING);
-      response.write('<li><a href="/">(root) /</a></li>', ENCODING);
-      response.write('<li><a href="../">(parent) ../</a></li>', ENCODING);
+      response.write('<li>\u{1F3E0}<a href="/">(root) /</a></li>', ENCODING);
+      response.write('<li>\u{1F53C}<a href="../">(parent) ../</a></li>', ENCODING);
       files.filter(isVisibleFile).forEach(writeLink, response);
       response.end('</ol>', ENCODING);
     }
@@ -53,27 +55,29 @@ function isVisibleFile(file) {
 }
 
 function writeLink(file) {
+  let icon = '\u{1F4C3}';
   try {
     let stat = fs.statSync(file);
     if (stat.isDirectory()) {
       file += '/';
+      icon = '\u{1F4C1}';
     }
   } catch (e) {
     // swallow (probably looking at a file in a hidden .folder path)
   }
-  this.write(`<li><a href="${file}">${file}</a></li>`, ENCODING);
+  this.write(`<li>${icon}<a href="${file}">${file}</a></li>`, ENCODING);
 }
 
 function getHeaders(localPath) {
   let ext = localPath ? path.extname(localPath).split('.').pop() : '';
   const CONTENT_TYPES = {
-    js: 'text/javascript',
-    json: 'application/json',
+    js: 'text/javascript; charset=utf-8',
+    json: 'application/json; charset=utf-8',
     png: 'image/png',
-    css: 'text/css',
-    html: 'text/html'
+    css: 'text/css; charset=utf-8',
+    html: 'text/html; charset=utf-8'
   };
   return {
-    'Content-Type': CONTENT_TYPES[(ext || '').toLowerCase()] || 'text/plain'
+    'Content-Type': CONTENT_TYPES[(ext || '').toLowerCase()] || 'text/plain; charset=utf-8'
   };
 }
